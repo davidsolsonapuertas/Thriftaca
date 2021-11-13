756938ada92c5da3456e4d373995f62563a98a64
davidsolsonapuertas <davidsolsonapuertas@gmail.com>
2021-11-13 14:45:37 -0500

    Initial Commit

---
+++ Thriftaca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thriftaca
+//
+//  Created by David Solsona on 13/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
